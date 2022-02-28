278e1702696e719a21e2d6eba8d584fab12cda6d
jonahdh <jonahdh@email.unc.edu>
2022-02-28 14:17:15 -0500

    Update flips.js

--- flips.js
+++ flips.js
@@ -3,3 +3,10 @@ import { coinFlips, countFlips} from "./modules/coin.mjs"
 const args = require('minimist')(process.argv.slice(2));
 
 args['number'];
+
+const flipNum = args['number'];
+const result = coinFlips(flipNum);
+const count = coinFlips(result);
+
+console.log(result);
+console.log(count);
